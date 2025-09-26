abb5848ae5644c701db8a765e7fba3d2a91d4eeb
Magdc <117609790+Magdc@users.noreply.github.com>
2025-09-26 10:19:07 -0500

    Refactor MiPrimerRobot and Racer classes to improve robot management and movement logic

--- Trabajos/MiPrimerRobot.java
+++ Trabajos/MiPrimerRobot.java
@@ -1,56 +1,12 @@
+
 import kareltherobot.*;
 import java.awt.Color;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
 public class MiPrimerRobot implements Directions{
-    private final List<Racer> racers = new ArrayList<>();
-/*public static void main(String [] args)
-{
-// Usamos el archivo que creamos del mundo
- World.readWorld("Mundo.kwld");
-World.setVisible(true);
-// Coloca el robot en la posición inicial del mundo (1,1),
-// mirando al Este, sin ninguna sirena.
-Robot Karel = new Robot(1, 1, East, 0);
-
-Robot azul = new Robot(1,1,East,0, Color.blue);
-
-// Mover el robot 4 pasos
-Karel.move();
- azul.move();
-Karel.move();
- azul.move();
-Karel.move();
- azul.move();
-Karel.move();
- azul.move();
-// Recoger los 5 beepers
-Karel.pickBeeper();
-Karel.pickBeeper();
-Karel.pickBeeper();
-Karel.pickBeeper();
-Karel.pickBeeper();
-// Girar a la izquierda y salir de los muros
-Karel.turnLeft();
- azul.turnLeft();
-Karel.move();
- azul.move();
-Karel.move();
- azul.move();
-// Poner los beepers fuera de los muros
-Karel.putBeeper();
-Karel.putBeeper();
-Karel.putBeeper();
-Karel.putBeeper();
-Karel.putBeeper();
-// Ponerse en otra posición y apagar el robot
-Karel.move();
- azul.move();
-Karel.turnOff();
- azul.turnOff();
-
-}*/
+    private static final List<Racer> racers = new ArrayList<>();
+    public static final TrafficController controller = new TrafficController(21, 31);
 
     // Crear los 28 robots que van en la zona azul
     // Las posiciones van de (1,7) a (4,1)
@@ -58,8 +14,8 @@ Karel.turnOff();
         for (int i = 1; i < 5 ; i++) {
             for (int j = 7; j > 0; j--) {
                 switch (i) {
-                    case 1,3: new Racer(i, j, East, 0, Color.blue).recorridoAzul(); break;
-                    case 2,4: new Racer(i, j, West, 0, Color.blue).recorridoAzul(); break;
+                        case 1,3: {racers.add(new Racer(i, j, East, 0, Color.blue));controller.ocuparPosicion(i,j);} break;
+                    case 2,4: {racers.add(new Racer(i, j, West, 0, Color.blue));controller.ocuparPosicion(i,j);} break;
                 }
             }
         }
@@ -70,10 +26,10 @@ Karel.turnOff();
             for (int j = 30; j > 22; j--) {
 
                 switch (i) {
-                    case 12:if (j == 28 || j == 29 )new Racer(i, j, West, 0, Color.green); break;
-                    case 13, 15: new Racer(i, j, West, 0, Color.green); break;
-                    case 14: new Racer(i, j, East, 0, Color.green); break;
-                    case 16: if(j==30 || j == 29) new Racer(i, j,West, 0, Color.green);
+                    case 12:if (j == 28 || j == 29 )racers.add(new Racer(i, j, West, 0, Color.green));controller.ocuparPosicion(i,j); break;
+                    case 13, 15: racers.add(new Racer(i, j, West, 0, Color.green));controller.ocuparPosicion(i,j); break;
+                    case 14: racers.add(new Racer(i, j, East, 0, Color.green));controller.ocuparPosicion(i,j); break;
+                    case 16: if(j==30 || j == 29) racers.add(new Racer(i, j,West, 0, Color.green));controller.ocuparPosicion(i,j);
                 }
             }
         }
@@ -81,14 +37,33 @@ Karel.turnOff();
     public static void main(String[] args) {
         World.readWorld("Mundo.kwld");
         World.setVisible(true);
-        World.setDelay(20); // opcional para ver la animación más clara
+        World.setDelay(3); // opcional para ver la animación más clara
         crearZonaAzul();
-        crearZonaVerde();
+        System.out.println(" //// PRIMERA VERSION DEL MAPA //// ");
+        controller.imprimirMapa();
+        // Iniciar todos los robots
+        for (Racer racer : racers) {
+            new Thread(racer).start();
+        }
+        //crearZonaVerde();
+        /*
+
+        Racer racer1 = new Racer(1,7,East,0,Color.blue);
+        Racer racer2 = new Racer(1,6,East,0,Color.blue);
+        Racer racer3 = new Racer(1,5,East,0,Color.blue);
+        //Racer racer2 = new Racer(7,2,East,Color.BLUE);
+        //Racer racer3 = new Racer(7,3,East,Color.YELLOW);
+
+        new Thread(racer1).start();
+        new Thread(racer2).start();
+        new Thread(racer3).start();
+        //new Thread(racer2).start();
+        //new Thread(racer3).start();
         // Dos robots en la misma posición y orientación
         //Robot first  = new Robot(1, 1, East, 0);               // rojo por defecto
         //Robot second = new Robot(1, 1, East, 0, Color.blue);   // azul
 
-        // Crea los hilos
+        // Crea los hilos/----
         //Thread t1 = new Thread(new RacerTask(first),  "Racer-1");
         //Thread t2 = new Thread(new RacerTask(second), "Racer-2");
 
--- Trabajos/Racer.java
+++ Trabajos/Racer.java
@@ -1,44 +1,118 @@
+
 import kareltherobot.*;
 import java.awt.Color;
 class Racer extends Robot implements Runnable {
     private int beepers;
     private Color color;
+    private Direction dir;
+    private int street;
+    private int avenue;
+    private int[][] ruta;
     // Constructor
-    public Racer(int street, int avenue, Direction direction, int beeps) {
+    public Racer(int street, int avenue, Direction direction, int beeps, Color color) {
         super(street, avenue, direction, beeps);
-        beepers = beeps;
+        this.beepers = beeps;
+        this.color = color;
+        this.dir= direction;
+        this.street = street;
+        this.avenue = avenue;
+        this.ruta = TrafficController.rutaRapidaAzul;
         World.setupThread(this);  // configura este robot para correr en un hilo
     }
 
-    public Racer(int street, int avenue, Direction direction, int beeps, Color color) {
+    public Racer(int street, int avenue, Direction direction, int beeps, Color color, int[][] ruta) {
         super(street, avenue, direction, beeps, color);
-        beepers = beeps;
+        this.beepers = beeps;
         this.color = color;
+        this.dir= direction;
+        this.street = street;
+        this.avenue = avenue;
+        this.ruta = ruta;
         World.setupThread(this);
+    }
+    // cálculo de la siguiente posición al moverse en sentido sur o norte
+    private int nextStreet() {
+        if (dir.equals(North)) {
+            return street + 1;
+        } else if (dir.equals(South)) {
+            return street - 1;
+        }
+        return street;
+    }
+    private void cambiarValoreStreet(){
+        if (dir.equals(North)) {
+            this.street = street + 1;
+        } else if (dir.equals(South)) {
+            this.street = street - 1;
+        }
+    }
+    private void cambiarValorAvenue(){
+        if (dir.equals(East)) {
+            this.avenue = avenue + 1;
+        } else if (dir.equals(West)) {
+            this.avenue = avenue - 1;
+        }
+    }
+
+    // calculo de la siguiente posición al moverse en sentido este u oeste
+    private int nextAvenue() {
+        if (dir.equals(East)) {
+            return avenue + 1;
+        } else if (dir.equals(West)) {
+            return avenue - 1;
+        }
+        return avenue;
+    }
+    public void giro() {
+        if (facingEast()) {
+            dir = East;
+        } else if (facingWest()) {
+            dir = West;
+        } else if (facingNorth()) {
+            dir = North;
+        } else if (facingSouth()) {
+            dir = South;
+        }
     }
 
     // Lógica del recorrido
     public void recorridoAzul() {
-        if (frontIsClear()){
-            move();
 
-        } else{
-            turnLeft();
-        }
-        if(nextToABeeper() &&  beepers< 4) {
-            pickBeeper();
-        }
+        System.out.println("Recorrido azul tratando de seguir aca");
+        seguirRuta(ruta);
     }
-    public void recorridoVerde() {
-        if (frontIsClear()){
-            move();
+    public void safeMove() {
+        MiPrimerRobot.controller.requestAndCommitMove(street, avenue, nextStreet(), nextAvenue(), this::move);
+    }
+    public void seguirRuta(int [][] ruta) {
+        for (int i = 0; i < ruta.length-1; i++) {
+            System.out.println("Llegue hasta aqui, voy a evaluar si "+
+                    ruta[i][0] + " == "+ avenue +" y "+ ruta[i][1]+ " == "+ street);
+            if ( avenue == ruta[i][0] && street == ruta[i][1]) {
+                System.out.println("entre");
+                System.out.println("Esta el frente sin nadie? " + frontIsClear() + " Proxima avenida: "
+                        + nextAvenue() + " == " + ruta[i + 1][0] + " y Proxima calle: " + nextStreet() + " == " + ruta[i + 1][1]);
+                if(nextToABeeper() &&  beepers< 4) {
+                    System.out.println("me enloqueci con los beepers");
+                    this.beepers = beepers + 1;
+                    pickBeeper();
+                    i = i-1;
+                    System.out.println("Tengo beepers: " + beepers);
+                }
+                else if (frontIsClear() && nextAvenue() == ruta[i + 1][0] && nextStreet() == ruta[i + 1][1]) {
+                    System.out.println("Me puedo mover");
+                    safeMove();
+                    System.out.println("Lo logre señor");
+                    cambiarValorAvenue();
+                    cambiarValoreStreet();
+                } else {
+                    i = i - 1;
+                    turnLeft();
+                    giro();
+                }
+            }
+        }
 
-        } else{
-            turnLeft();
-        }
-         if(nextToABeeper() && beepers < 4) {
-            pickBeeper();
-        }
     }
 
     // Método que arranca el hilo
@@ -47,7 +121,7 @@ class Racer extends Robot implements Runnable {
         if (color == Color.blue) {
             recorridoAzul();
         }else{
-            recorridoVerde();
+            //recorridoVerde();
         }
     }
 }
